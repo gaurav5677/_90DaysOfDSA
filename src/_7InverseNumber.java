54e02b8baab86f3526f325a222c07c0b84d8b2d3
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-27 11:35:43 +0530

    Day 1 (InverseANumber)

--- src/_7InverseNumber.java
+++ src/_7InverseNumber.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class _7InverseNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner( System.in);
+         int n = s.nextInt();
+
+
+          int inv = 0 ;
+          int  op = 1 ; //orignal place
+        while( n !=0){
+            int od = n%10; //original digit
+            int id = op; // inverse digit
+            int ip = od ;// inverse place
+
+
+            // make change to inv using ip and id
+            inv   = inv + id * (int)Math.pow( 10,ip-1);
+
+
+            n = n/10;
+            op++;
+
+        }
+        System.out.println(inv);
+    }
 }
