990a42414e4008393f9a6e71b240b71239014fe8
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-09-17 00:26:40 +0530

    PrintAll fibonacci Number

--- src/_3printAllFibonacciNumberTillN.java
+++ src/_3printAllFibonacciNumberTillN.java
@@ -9,7 +9,8 @@ public class _3printAllFibonacciNumberTillN {
 
         int a = 0 ,b=1;
         for( int i =0 ; i<=x;i++){
-            System.out.println(i);
+
+            System.out.println(a);
             int c = a+b;
 
            a=b;
